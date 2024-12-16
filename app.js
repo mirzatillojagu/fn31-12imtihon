2b6e19236cf0566290fded172d4e52430bd77883
mirzatillojagu <imirzatillo0@gmail.com>
2024-12-16 14:48:31 +0500

    vazifa

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ import { createCard } from "./function.js";
 
 const input = document.querySelector("#input");
 const button = document.querySelector(".search-img");
-const wrapper=document.querySelector('.wrapper')
+const wrapper = document.querySelector(".wrapper");
 
-
-button &&  button.addEventListener("click", function () {
+button && button.addEventListener("click", function () {
     let sum = input.value;
     fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${sum}`, {
       method: "GET",
@@ -17,34 +16,32 @@ button &&  button.addEventListener("click", function () {
       })
       .then((data) => {
         console.log(data);
-        let card = createCard
+        let card = createCard;
         wrapper.innerHTML = card;
       })
       .catch((error) => {
         console.log(error.message);
-        alert(error)
+        alert(error);
       });
   });
 
-  const btn = document.querySelector(".DayNight");
-  const img = document.querySelector(".oval-img");
-  const body = document.body;
+const btn = document.querySelector(".DayNight");
+const img = document.querySelector(".oval-img");
+const body = document.body;
 
-  btn.addEventListener("click", function () {
-    if (body.style.backgroundColor === "black") {
-      body.style.backgroundColor = "white";
-      body.style.color = "black";
-    } else {
-      body.style.backgroundColor = "black";
-      body.style.color = "white";
-    }
-  
-    if (img.style.marginLeft === "4px") {
-      img.style.marginLeft = "-15px";
-    } else {
-      img.style.marginLeft = "4px";
-    }
-  });
-  
+btn.addEventListener("click", function () {
+  if (body.style.backgroundColor === "black") {
+    body.style.backgroundColor = "white";
+    body.style.color = "black";
+  } else {
+    body.style.backgroundColor = "black";
+    body.style.color = "white";
+  }
 
-
+  if (img.style.marginLeft === "4px") {
+    img.style.marginLeft = "-16px";
+    img.style.padding = "1px";
+  } else {
+    img.style.marginLeft = "4px";
+  }
+});
