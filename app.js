5697771c1e401918006b058db7af306dcfee6d75
mirzatillojagu <imirzatillo0@gmail.com>
2024-12-16 17:36:13 +0500

    imtihon

--- app.js
+++ app.js
@@ -3,31 +3,51 @@ import { createCard } from "./function.js";
 const input = document.querySelector("#input");
 const button = document.querySelector(".search-img");
 const wrapper = document.querySelector(".wrapper");
+const body = document.body;
+const fontSelector = document.querySelector("#font-selector");
+
+fontSelector.addEventListener("change", function () {
+    const selectedFont = fontSelector.value; 
+    body.style.fontFamily = selectedFont;
+});
 
 button && button.addEventListener("click", function () {
     let sum = input.value;
+
+    if (sum === "") {
+      alert("Iltimos, qidirish uchun so'z kiriting.");
+      return;
+    }
+
+    const isValidWord = /^[a-zA-Z]+$/.test(sum);
+    if (!isValidWord) {
+      alert("Iltimos, toʻgʻri soʻz kiriting (faqat harflar).");
+      return;
+    }
+
     fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${sum}`, {
       method: "GET",
     })
       .then((response) => {
-        if (response.status == 200) {
+        if (response.status === 200) {
           return response.json();
+        } else {
+          throw new Error("Maʼlumotlarni olib boʻlmadi");
         }
       })
       .then((data) => {
         console.log(data);
-        let card = createCard;
+        let card = createCard(data);
         wrapper.innerHTML = card;
       })
       .catch((error) => {
         console.log(error.message);
-        alert(error);
+        alert("So'z topilmadi yoki xatolik yuz berdi.");
       });
   });
 
 const btn = document.querySelector(".DayNight");
 const img = document.querySelector(".oval-img");
-const body = document.body;
 
 btn.addEventListener("click", function () {
   if (body.style.backgroundColor === "black") {
@@ -39,9 +59,4 @@ btn.addEventListener("click", function () {
   }
 
   if (img.style.marginLeft === "4px") {
-    img.style.marginLeft = "-16px";
-    img.style.padding = "1px";
-  } else {
-    img.style.marginLeft = "4px";
-  }
-});
+    img.style.marginLeft = "-16px
